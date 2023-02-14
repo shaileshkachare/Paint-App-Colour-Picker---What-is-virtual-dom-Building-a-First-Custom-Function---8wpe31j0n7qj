32a57e2d68e9fffeefb665ac36ae82fedf1e9e2e
shaileshkachare <shaileshkachare2@gmail.com>
2023-02-14 19:04:11 +0530

    Assignment

--- src/components/Selection.js
+++ src/components/Selection.js
@@ -1,1 +1,25 @@
 
+import React from "react"
+import "../styles/Child.css"
+
+class Selection extends React.Component{
+    constructor(){
+        super()
+        this.state={
+            selectionStyle:{background:''}
+        }
+    }
+    updateSelectionStyle=(value)=>{
+        this.setState({selectionStyle: value})
+    }
+    render(){
+        return <div className="fix-box" style={this.state.selectionStyle} onClick={()=>{
+            // this.setState({selectionStyle: this.props.nextBackground})
+            this.props.applyColor(this.updateSelectionStyle)
+        }}>
+            <h2 className="subheading">Selection</h2>
+        </div>
+    }
+}
+
+export default Selection
